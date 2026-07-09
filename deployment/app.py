fe601536d6218e1d64bdb8ca3f1c91e8d2973f7c
ishekaa12 <ishekasingh26@gmail.com>
2026-07-09 16:32:36 +0530

    chore: add render.yaml, expose PORT env variable, include models for deployment

--- deployment/app.py
+++ deployment/app.py
@@ -310,4 +310,5 @@ def generate_answer(question):
                 "Please ask about one of these topics.")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
